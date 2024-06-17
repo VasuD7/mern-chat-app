ccd3e485f1fecc2caa0f02528a931cbf93d3d5e4
CodeBoyD7 <vasudeva2002y@gmail.com>
2024-06-18 02:14:11 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:4000", {
+      const socket = io("https://mern-chat-prod-xbyc.onrender.com/", {
         query: {
           userId: authUser._id,
         },
